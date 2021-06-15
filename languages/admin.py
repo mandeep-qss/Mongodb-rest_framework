94e5a852069d341f331a9ba6d504bd824f9d983d
mandeep-qss <mandeep.kaur@qsstechnosoft.com>
2021-06-15 11:46:26 +0530

    First Commit

--- languages/admin.py
+++ languages/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from languages.models import Language, Paradigm, Programmer
+from languages.models import Language, Paradigm, Programmer, Book, Author
 
 # Register your models here.
 
 admin.site.register(Paradigm)
 admin.site.register(Language)
 admin.site.register(Programmer)
+admin.site.register(Book)
+admin.site.register(Author)
